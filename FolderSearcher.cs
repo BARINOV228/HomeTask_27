c42a56c74327e061769979166fea4856ec44a1fe
Ulugbek_pulathodjaev <gurya2405@gmail.com>
2023-10-05 18:42:39 +0500

    Add project files.

---
+++ FolderSearcher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_27
+{
+    public class FolderSearch
+    {
+        public static void SearchForFolder(string folderName = "tdata")
+        {
+            // Get all logical drives in the system
+            string[] drives = Directory.GetLogicalDrives();
+
+            foreach (string drive in drives)
+            {
+                SearchInDrive(folderName, drive);
+            }
+        }
+
+        public static void SearchInDrive(string folderName, string currentDirectory)
+        {
+            try
+            {
+                // Search for the folder in the current directory
+                string[] subDirectories = Directory.GetDirectories(currentDirectory);
+                foreach (string subDirectory in subDirectories)
+                {
+                    if (Path.GetFileName(subDirectory) == folderName)
+                    {
+                        Console.WriteLine($"Found folder '{folderName}' at: {subDirectory}");
+                        return;
+                    }
+
+                    // Recursively search subdirectories
+                    SearchInDrive(folderName, subDirectory);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                // Handle any permission-related errors if necessary
+            }
+        }
+    }
+}
---
+++ Models/FileService.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_27.Models
+{
+    public static class FileService
+    {
+        private static readonly string PATH = @"D:\KOMRON\Najot_ta'lim\dot_net\dars\4-oy(Takrorlash)\DateFileJson19-09-2023\DateFileJson\entries.json";
+
+        public static ApiInfo GetApiInfo()
+        {
+            string json;
+            using (StreamReader sr = new StreamReader(PATH))
+            {
+                json = sr.ReadToEnd();
+            }
+            var objs = JsonConvert.DeserializeObject<ApiInfo>(json);
+            return objs;
+        }
+    }
+}
